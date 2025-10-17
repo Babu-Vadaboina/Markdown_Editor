621270dd344fc3b2ec2b678f93878e0b3255cf0f
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-10-17 21:49:45 +0530

    added preview pane logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
+import Preview from "./components/Preview";
 
 function App() {
   const [markdown, setMarkdown] = useState("# HelloWorld");
   return (
     <>
-      <div className="flex h-screen">
-        <div className="w-1/2">
+      <div className="flex h-screen p-4 gap-4">
+        <div className="flex-1">
           <Editor value={markdown} onChange={setMarkdown} />
         </div>
-        <div className="w-1/2"></div>
+        <div className="flex-1">
+          <Preview markdown={markdown} />
+        </div>
       </div>
     </>
   );
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -4,3 +4,18 @@ import ReactMarkdown from "react-markdown";
 interface PreviewProps {
   markdown: string;
 }
+
+const Preview: React.FC<PreviewProps> = ({ markdown }) => {
+  return (
+    <div className="flex flex-col h-full border border-gray-300 rounded-lg">
+      <div className="bg-gray-100 px-4 py-2 font-semibold border-b text-gray-700">
+        Preview 👀
+      </div>
+      <div className="flex-1 p-3 overflow-y-auto bg-white prose">
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+export default Preview;
